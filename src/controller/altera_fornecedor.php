b7fd3e1ccc7451c3f6253a251bfafbe8d8d6c1c1
DouglasGarcez <52336946+DouglasGarcez@users.noreply.github.com>
2021-06-16 23:01:55 -0300

    Tive que mudar a estruturação do sistema por conta das imagens dos produtos

---
+++ src/controller/altera_fornecedor.php
@@ -0,0 +1,22 @@
+<?php
+include_once "../fachada.php";
+
+$id = @$_GET['id'];
+$nome = @$_POST["nome"];
+$descricao = @$_POST["descricao"];
+$email = @$_POST["email"];
+$telefone = @$_POST["telefone"];
+
+if (empty($nome) || empty($email) || empty($telefone) || empty($descricao)){
+    echo "<script type=\"text/javascript\">alert('Voce nao preencheu todos os campos, verifique novamente!')</script>";
+    exit;
+}
+
+$fornecedor = new Fornecedor($id, $nome, $descricao, $telefone, $email);
+$dao = $factory->getFornecedorDao();
+$dao->altera($fornecedor);
+
+header("Location: ../view/fornecedor.php");
+exit;
+
+?>
